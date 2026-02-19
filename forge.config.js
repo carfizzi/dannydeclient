18f5d634159aeaffb073aa6b2da4b78c0fae1918
Marco <marco.carfizzi@gmail.com>
2026-02-19 18:19:45 +0100

    fix: linux appimage builder

--- forge.config.js
+++ forge.config.js
@@ -1,63 +1,66 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const {FusesPlugin} = require('@electron-forge/plugin-fuses');
+const {FuseV1Options, FuseVersion} = require('@electron/fuses');
 
 module.exports = {
-  packagerConfig: {
-    asar: true,
-    icon: './image'
-  },
-  rebuildConfig: {},
-  makers: [
-    // Only build installer on Windows or if Wine is installed (skipped for macOS without Wine)
-    ...(process.platform === 'win32' ? [{
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        setupIcon: './image.ico'
-      },
-    }] : []),
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin', 'win32', 'linux'],
+    packagerConfig: {
+        asar: true,
+        icon: './image'
     },
-    ...(process.platform === 'linux' ? [
-      {
-        name: '@electron-forge/maker-deb',
-        config: {
-          options: {
-            icon: './image.png'
-          }
+    rebuildConfig: {},
+    makers: [
+        // Only build installer on Windows or if Wine is installed (skipped for macOS without Wine)
+        ...(process.platform === 'win32' ? [{
+            name: '@electron-forge/maker-squirrel',
+            config: {
+                setupIcon: './image.ico'
+            },
+        }] : []),
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['darwin', 'win32', 'linux'],
         },
-      },
-      {
-        name: '@electron-forge/maker-rpm',
-        config: {
-          options: {
-            icon: './image.png'
-          }
+        ...(process.platform === 'linux' ? [
+            {
+                name: '@electron-forge/maker-deb',
+                config: {
+                    options: {
+                        icon: './image.png'
+                    }
+                },
+            },
+            {
+                name: '@electron-forge/maker-rpm',
+                config: {
+                    options: {
+                        icon: './image.png'
+                    }
+                },
+            },
+            {
+                name: 'electron-forge-maker-appimage',
+                config: {
+                    options: {
+                        icon: './image.png'
+                    }
+                },
+            }
+        ] : []),
+    ],
+    plugins: [
+        {
+            name: '@electron-forge/plugin-auto-unpack-natives',
+            config: {},
         },
-      },
-      // AppImage works across most Linux distros (including Arch) without install
-      {
-        name: 'electron-forge-maker-appimage',
-        config: {},
-      }
-    ] : []),
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
-    new FusesPlugin({
-      version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
-    }),
-  ],
+        // Fuses are used to enable/disable various Electron functionality
+        // at package time, before code signing the application
+        new FusesPlugin({
+            version: FuseVersion.V1,
+            [FuseV1Options.RunAsNode]: false,
+            [FuseV1Options.EnableCookieEncryption]: true,
+            [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+            [FuseV1Options.EnableNodeCliInspectArguments]: false,
+            [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+            [FuseV1Options.OnlyLoadAppFromAsar]: true,
+        }),
+    ],
 };
